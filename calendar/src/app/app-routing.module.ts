fa359254ab360d79f0a1687a9a26b88467f082ab
WillianCosta <willian95@outlook.com>
2024-01-17 23:19:15 -0300

    feat: add navbar

--- calendar/src/app/app-routing.module.ts
+++ calendar/src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './screens/calendar/calendar.component';
 
-const routes: Routes = [{ path: '', component: CalendarComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: CalendarComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
