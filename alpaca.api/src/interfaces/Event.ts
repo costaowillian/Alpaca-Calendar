b596cd06e4db21854e115d505e570cd6cd8f7758
WillianCosta <willian95@outlook.com>
2024-01-18 14:18:59 -0300

    feat: add interfaces

---
+++ alpaca.api/src/interfaces/Event.ts
@@ -0,0 +1,6 @@
+export interface IEvet {
+  description: string;
+  start: string;
+  end: string;
+  userId: string;
+}
---
+++ alpaca.api/src/models/Protocol.ts
@@ -0,0 +1,23 @@
+export interface HttpResponse<T> {
+  statusCode: httpStatusCode;
+  body: T;
+}
+
+export enum httpStatusCode {
+  OK = 200,
+  BAD_REQUEST = 400,
+  UNAUTHORIZED = 401,
+  NOTFOUND = 404,
+  SERVER_ERROR = 500,
+  CREATED = 201
+}
+
+export interface HttpRequest<B> {
+  params?: any;
+  headers?: any;
+  body?: B;
+}
+
+export interface IController {
+  handle(httpRequest: HttpRequest<unknown>): Promise<HttpResponse<unknown>>;
+}
