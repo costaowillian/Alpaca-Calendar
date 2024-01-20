274f404bfed620e4c444874c6170c154e00938a5
WillianCosta <willian95@outlook.com>
2024-01-20 19:28:34 -0300

    refactor: auth service

--- calendar/src/app/services/auth.service.ts
+++ calendar/src/app/services/auth.service.ts
@@ -11,21 +11,25 @@ export class AuthService {
   criptKey: string = environment.apiKey;
   constructor(private userService: UserService) {}
 
+  // Método para criptografar texto usando a chave
   private encrypt(txt: string): string {
     return CryptoJS.AES.encrypt(txt, this.criptKey).toString();
   }
 
+  // Método para descriptografar texto usando a chave
   private decrypt(txtToDecrypt: string): string {
     return CryptoJS.AES.decrypt(txtToDecrypt, this.criptKey).toString(
       CryptoJS.enc.Utf8
     );
   }
 
-  getAuthorizationToken(item:string): string {
+  // Obter token de autorização descriptografado do armazenamento local
+  getAuthorizationToken(item: string): string {
     const localItem = this.decrypt(window.localStorage.getItem(item) || '');
     return localItem;
   }
 
+  // Autenticar usuário, armazenar token criptografado e ID do usuário no armazenamento local
   async authUser(user: IUserCredentials): Promise<boolean> {
     const userData = await this.userService.getUserData(user);
     if (userData && typeof userData !== 'string') {
@@ -37,11 +41,13 @@ export class AuthService {
     return false;
   }
 
+  // Remover token e ID do usuário do armazenamento local ao fazer logout
   logout() {
     window.localStorage.removeItem('token');
     window.localStorage.removeItem('user');
   }
 
+  // Verificar se o usuário está autenticado com base na presença do token
   isUserLoggedIn() {
     const token = this.getAuthorizationToken('token');
     if (!token) {
