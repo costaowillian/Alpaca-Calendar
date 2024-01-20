aa642b5921aa129a9e56d141f1e0e2c4d716b96b
WillianCosta <willian95@outlook.com>
2024-01-20 15:01:15 -0300

    feat: add guard

--- calendar/src/app/app-routing.module.ts
+++ calendar/src/app/app-routing.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './screens/calendar/calendar.component';
 import { AccessPageComponent } from './screens/access-page/access-page.component';
+import { GuardGuard } from './guard/auth.guard';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: CalendarComponent },
+  {
+    path: '',
+    component: CalendarComponent,
+    canActivate: [GuardGuard],
+    children: [{ path: 'home', component: CalendarComponent }],
+  },
   { path: 'access-page', component: AccessPageComponent },
+  { path: '**', redirectTo: 'access-page' }, // Redireciona para 'access-page' se a rota não for reconhecida
 ];
 
 @NgModule({
